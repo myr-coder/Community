46bf6296eb5198d0c5f830e33d5c17a55780ac7d
myr-coder <merylyroberts@gmail.com>
2023-09-03 23:16:46 +0530

    first

---
+++ script.js
@@ -0,0 +1,313 @@
+
+
+// All quotes, authors and their images
+const quotes = [
+{ quote: "“I regret nothing.”",
+  author: "- Jeffrey Winger",
+  image: "jeff.png"},
+{ quote: "“To me, religion is like Paul Rudd. I see the appeal, and I would never take it away from anyone, but I would also never stand in line for it.”",
+  author: "- Jeffrey Winger",
+  image: "jeff.png"  },
+{ quote: "“I discovered at a very early age that if I talked long enough, I could make anything right or wrong. So either I'm God, or truth is relative. Either way: Booyah.”",
+  author: "- Jeffrey Winger",
+  image: "jeff.png"  },
+{ quote: "“The funny thing about being smart, is that you can get through most of life without ever having to do any work.”",
+  author: "- Jeffrey Winger",
+  image: "jeff.png"  },
+{ quote: "“Why are we in such a rush to leave the tide pool when the only things waiting for us on shore are the sands of time and the hungry seagulls of slowly growing apart?”",
+  author: "- Jeffrey Winger",
+  image: "jeff.png"  },
+{ quote: "“Don't talk to me about romance. I had a three way in a hot air balloon.”",
+  author: "- Jeffrey Winger",
+  image: "jeff.png"  },
+{ quote: "“Harrison Ford is irradiating our testicles with microwave satellite transmissions!”",
+  author: "- Jeffrey Winger",
+  image: "jeff.png"  },
+{ quote: "“You are all better than you think you are, you are just designed not to believe it when you hear it from yourself.”",
+  author: "- Jeffrey Winger",
+  image: "jeff.png"  },
+{ quote: "“TV's the best dad there is. TV never came home drunk, TV never forgot me at the zoo, TV never abused and insulted me.. unless you count Cop Rock.”",
+  author: "- Jeffrey Winger",
+  image: "jeff.png"  },
+{ quote: "“I'm Neo in the third act of The Matrix. I'm also Neo in the first act of the second Matrix.”",
+  author: "- Jeffrey Winger",
+  image: "jeff.png"  },
+{ quote: "“I always go the extra mile to avoid doing things.”",
+  author: "- Jeffrey Winger",
+  image: "jeff.png"  }, 
+{ quote: "“With all due respect sir, I have zero respect for you.”",
+  author: "- Jeffrey Winger",
+  image: "jeff.png"  }, 
+{ quote: "“Time flies when you're joking around about movie titles that sound like names for poop.”",
+  author: "- Jeffrey Winger",
+  image: "jeff.png"  },
+{ quote: "“You know who has real conversations? Ants. They talk by vomiting chemicals in each other's mouths. They get right down to brass tacks.”",
+  author: "- Jeffrey Winger",
+  image: "jeff.png"},
+{ quote: "“I don't have an ego. My Facebook photo is a landscape.”",
+  author: "- Jeffrey Winger",
+  image: "jeff.png"},
+
+
+{ quote: "“Sometimes the hardest prisons to break out of, are the ones without locks.”",
+  author: "- Abed Nadir", 
+  image: "abed.png" },
+{ quote: "“Our first assignment is a documentary. The're like real movies, but with ugly people.”",
+  author: "- Abed Nadir",
+  image: "abed.png"  },
+{ quote: "“I looked inside Nicolas Cage and I found a secret: People are random and pointless.”",
+  author: "- Abed Nadir",
+  image: "abed.png"  },
+{ quote: "“I've got self-esteem falling out of my butt.”",
+  author: "- Abed Nadir",
+  image: "abed.png"   },
+{ quote: "“Did you know you can make napalm out of common dish soap and cat food?”",
+  author: "- Abed Nadir",
+  image: "abed.png"    },
+{ quote: "“When you really know who you are and what you like about yourself, changing for other people isn't such a big deal.”",
+  author: "- Abed Nadir",
+  image: "abed.png"    },
+{ quote: "“Draw the tape worm out of him with the bowl of milk that is your sexuality.”",
+  author: "- Abed Nadir",
+  image: "abed.png"   },
+{ quote: "“The universe is an endless raging sea of randomness. Our job isn't to fight it, but to weather it together, on the raft of life.”",
+  author: "- Abed Nadir",
+  image: "abed.png"   },
+{ quote: "“This is definitely the darkest timeline.”",
+  author: "- Evil Abed",
+  image: "abed.png"    },
+{ quote: "“When the world gets bad enough, the good go crazy. But the smart.. they go bad.”",
+  author: "- Evil Abed",
+  image: "abed.png"   },
+
+
+{ quote: "“He was horny, so he dropped him. Man is evil!”",
+  author: "- Annie Edison",
+  image: "annie.png"  },
+{ quote: "“I was so unpopular in high school, the crossing guards used to lure me into traffic.”",
+  author: "- Annie Edison",
+  image: "annie.png"  },
+{ quote: "“Everybody loves pelicans, they bring babies!”",
+  author: "- Annie Edison",
+  image: "annie.png"  },
+{ quote: "“Freeze Mamma-Jamma!”",
+  author: "- Annie Edison",
+  image: "annie.png"  }, 
+{ quote: "“I'm Annie Edison, but people call me Psycho because I had a nervous breakdown in high school.”",
+  author: "- Annie Edison",
+  image: "annie.png"  }, 
+{ quote: "“A passing grade? Like a C? Why don't I just get pregnant at a bus station?”",
+  author: "- Annie Edison",
+  image: "annie.png"  },
+{ quote: "“I'm a werewolf who feeds on selfish vampires!”",
+  author: "- Annie Edison",
+  image: "annie.png"  }, 
+
+
+{ quote: "“Never change, or do. I'm not your boss.”",
+  author: "- Troy Barnes",
+  image: "troy.png" },
+{ quote: "“I'm going to eat spaceman paninis with black Hitler and there's nothing you can do about it!”",
+  author: "- Troy Barnes",
+  image: "troy.png"  }, 
+{ quote: "“I don't get history. If I wanted to know what happened in Europe a long time ago, I'd watch Game of Thrones.”",
+  author: "- Troy Barnes",
+  image: "troy.png"  },
+{ quote: "“I wanna see if wiener dogs are born that way, or if they start off normal and then get wiener.”",
+  author: "- Troy Barnes",
+  image: "troy.png"  },
+{ quote: "“If it was cool to eat God, he'd be a chicken finger.”",
+  author: "- Troy Barnes",
+  image: "troy.png"  },
+{ quote: "“Do you get paid more if they do stuff to your butt?”",
+  author: "- Troy Barnes",
+  image: "troy.png"  },
+{ quote: "“If we get bit, we could turn into a zombie. The banana said so.”",
+  author: "- Troy Barnes",
+  image: "troy.png"  },
+{ quote: "“Do they find thoughts in our butts?”",
+  author: "- Troy Barnes",
+  image: "troy.png" },
+{ quote: "“They're making us walk around with pretzels in our butts, and I put mustard on mine like an idiot.”",
+  author: "- Troy Barnes",
+  image: "troy.png"  },
+{ quote: "“Sometimes I think I lost something really important to me, and then it turns out I already ate it.”",
+  author: "- Troy Barnes",
+  image: "troy.png"  },
+{ quote: "“I hope I get multiple personalities. I get lonely in long showers.”",
+  author: "- Troy Barnes",
+  image: "troy.png"  },
+{ quote: "“First time I was punched in the face, I was like ”Oh no!”, but then I was like ”This is a story..””",
+  author: "- Troy Barnes",
+  image: "troy.png"  },
+{ quote: "“All difficult things are better. Like carrying a disease. Or holding a fart right now.”",
+  author: "- Troy Barnes",
+  image: "troy.png" },
+{ quote: "“MOM! How many lies have I been living?”",
+  author: "- Troy Barnes",
+  image: "troy.png" },
+{ quote: "“I didn't get Inception. I didn't get Inception!”",
+  author: "- Troy Barnes",
+  image: "troy.png" },
+{ quote: "“Do you think bees eat their own honey?”",
+  author: "- Troy Barnes",
+  image: "bee-troy.png" },
+
+
+{ quote: "“A lifetime of dissapointment has given me douche-ray vision.”",
+  author: "- Britta Perry",
+  image: "britta.png"  },
+{ quote: "“I know what a metaphor is! It's like a thought with another thought's hat on.”",
+  author: "- Britta Perry",
+  image: "britta.png"  },
+{ quote: "“Blaming a bridge collapse on a school is like blaming owls for why I suck at analogies.”",
+  author: "- Britta Perry",
+  image: "britta.png"  },
+{ quote: "“Pizza, pizza, go in tummy, me so hungee, me so hungee!”",
+  author: "- Britta Perry",
+  image: "britta.png"  },
+{ quote: "“I refused to give Santa a Christmas List because I didn't want to depend on a man for anything.”",
+  author: "- Britta Perry",
+  image: "britta.png"  },
+{ quote: "“If I wanted the government in my uterus, I'd fill it with oil and Hispanic voters.”",
+  author: "- Britta Perry",
+  image: "britta.png"  },
+{ quote: "“The real reason men fight is to release their pent-up gayness.”",
+  author: "- Britta Perry" ,
+  image: "britta.png" },
+{ quote: "“Maybe, if you put stain remover on a turd, you don't get a diamond. You just get a turd, with less direction in life”",
+  author: "- Britta Perry",
+  image: "britta.png"  },
+{ quote: "“I am not a whore, and not that I've done the math, but if I were, I'd be the super classy kind who gets flown to Dubai to stay in an underwater hotel.”",
+  author: "- Britta Perry",
+  image: "britta.png"  },
+{ quote: "“When you become roommates with friends, the things you love about them become the things that make you want to smother them with a pillow.”",
+  author: "- Britta Perry",
+  image: "britta.png"  },
+{ quote: "“Calling for help? A classic...call for help.”",
+  author: "- Britta Perry",
+  image: "britta.png"  },
+{ quote: "“Feast your ear tongues on these memory pops.”",
+  author: "- Britta Perry",
+  image: "britta.png"  },
+
+  
+{ quote: "“Being a virgin in this day and age is something to be proud of. You're like a unicorn.”",
+  author: "- Shirley Bennett",
+  image: "shirley.png"  },
+{ quote: "“Kind people are always kind, not just when it's easy.”",
+  author: "- Shirley Bennett",
+  image: "shirley.png"  },
+{ quote: "“Welcome to Shirley Island, where all your dreams come true! If you dream of standing on a table and pissing in a jar.”",
+  author: "- Shirley Bennett",
+  image: "shirley.png"  },
+{ quote: "“I thought pushing that woman's head through a jukebox would make me feel better, but I'm just out $54...and a marriage.”",
+  author: "- Shirley Bennett",
+  image: "shirley.png"  },
+{ quote: "“What happened is between us and Jesus. And Jesus don't snitch.”",
+  author: "- Shirley Bennett",
+  image: "shirley.png"  },
+{ quote: "“Once you make a boy pee his pants, you think about where you life is headed.”",
+  author: "- Shirley Bennett",
+  image: "shirley.png"  },
+{  quote: "“I haven't felt that chemistry coming my way. I don't know if it's because you're racist or because I intimidate you sexually, but I know it's one of those two.”",
+  author: "- Shirley Bennett",
+  image: "shirley.png"  },
+
+
+{ quote: "“When we seek to destroy others, we often hurt ourselves, because it's the self that wants to be destroyed.”",
+  author: "- Pierce Hawthorne",
+  image: "pierce.png"  },
+{ quote: "“We had a re-birthing ritual in my friend's hot tub. I'm now a level five laser lotus in my Buddhist community.”",
+author: "- Pierce Hawthorne",
+image: "pierce.png" },
+{ quote: "“Let's burn this mother down!”",
+author: "- Pierce Hawthorne",
+image: "pierce.png" },
+
+
+{ quote: "“Fire can't go through doors, stupid. It's not a ghost.”",
+author: "- Ben Chang",
+image: "chang.png" },
+{ quote: "“I ate my twin in utero.”",
+author: "- Ben Chang",
+image: "chang.png" },
+{ quote: "“Boop! Boop! My bullcrap meter is going crazy!”",
+author: "- Ben Chang",
+image: "chang.png" },
+{ quote: "“Let me rest gently on your pecs.”",
+author: "- Ben Chang",
+image: "chang.png" },
+{ quote: "“Ham, girl!!”",
+author: "- Ben Chang",
+image: "chang.png" },
+
+
+{ quote: "“GAAYYY MARRIAAGEE!!”",
+author: "- Dean Pelton",
+image: "dean.png" },
+{ quote: "“Pilates is the demon that eats your genitals.”",
+author: "- Dean Pelton",
+image: "dean.png" },
+{ quote: "“Would that this hoodie were a time hoodie!”",
+author: "- Dean Pelton",
+image: "dean.png" },
+{ quote: "“This better not awaken anything in me.”",
+author: "- Dean Pelton",
+image: "dean.png" },
+{ quote: "“Frankly, my dear, I don't give a dean.”",
+author: "- Dean Pelton",
+image: "dean.png" },
+];
+
+const quoteElement = document.getElementById("quotes");
+const authorElement = document.getElementById("author");
+const characterElement = document.getElementById("characters");
+const generateButton = document.getElementById("generate");
+
+// Function 
+function generateQuote() {
+    const randomIndex = Math.floor(Math.random() * quotes.length);
+    const randomQuote = quotes[randomIndex];
+    quoteElement.textContent = randomQuote.quote;
+    authorElement.textContent = randomQuote.author;
+    characterElement.src = randomQuote.image;
+    characterElement.alt = `${randomQuote.author} Image`;
+
+    const colors = [ "#D99AA5", "#809bce", "#B37F88", "#95A686", "#65A695", "#BFBD97", "#D4AFB9", "#D3AB9E", "#A7BED3", "D9DAD9", "F1EEEB", "A8B1B0", "BBC0BD", "93B5A6"]
+    const randomColorIndex = Math.floor(Math.random() * colors.length);
+    document.body.style.backgroundColor = colors[randomColorIndex];
+}
+
+generateButton.addEventListener("click", ()=>{
+    // Simulate an error message sometimes for humor
+    if (Math.random() < 0.1) {
+        errorMessage.textContent = "Did I do that??";
+        errorMessage.classList.remove("hidden");
+        setTimeout(()=>{
+            errorMessage.classList.add("hidden");
+        }
+        , 3000);
+    } else {
+        errorMessage.classList.add("hidden");
+        generateQuote();
+    }
+
+}
+);
+
+// Event listener for the Generate button
+generateButton.addEventListener("click", generateQuote);
+
+// Initial generation on page load
+generateQuote();
+
+
+
+
+
+
+
+
+
+
